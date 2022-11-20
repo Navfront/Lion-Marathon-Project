472784af1c75c10a4dc58b6938d45c94f389d1b6
Navfront <b3rt@yandex.ru>
2022-11-20 15:11:11 +0300

    Updates

---
+++ script.js
@@ -0,0 +1,23 @@
+const MOV_MULTIPLIER = 4000;
+const INITIAL_WRAPPER_TRANSFORM = [-57, -55];
+const INITIAL_RECT_TRANSFORM = [-50,-50];
+
+const background = document.querySelector('.promo__bg');
+const titleWrapper = document.querySelector('.promo__title-wrapper');
+const rect = titleWrapper.querySelector('.promo__lion-rect');
+
+window.addEventListener('mousemove', mouseMoveHandler);
+
+function calcMovement(event, multiplier) {
+  const moveBgX = (window.outerWidth / 1.4 - event.clientX) / multiplier;
+  const moveBgY = (window.outerHeight / 1.4 - event.clientY) / multiplier;
+  return {moveBgX, moveBgY};
+}
+
+function mouseMoveHandler(event) {
+  const {moveBgX, moveBgY} = calcMovement(event, MOV_MULTIPLIER);
+  background.style.transform = `translate(${moveBgX}%,${moveBgY}%) scale(1.01)`;
+  titleWrapper.style.transform = `translate(${moveBgX / 2 + INITIAL_WRAPPER_TRANSFORM[0]}%,${moveBgY / 2 + INITIAL_WRAPPER_TRANSFORM[1]}%)`;
+  rect.style.transform = `translate(${moveBgX / 0.5 + INITIAL_RECT_TRANSFORM[0]}%,${moveBgY / 0.5 + INITIAL_RECT_TRANSFORM[1]}%)`;
+
+}
